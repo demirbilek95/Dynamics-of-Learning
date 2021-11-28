fef2cc73e3abe72c8776b6d0092084c13e8fc7f2
demirbilek95 <dogancandemirbilek@gmail.com>
2021-11-28 23:13:07 +0100

    fixed hyperparameter tuning function

--- scripts/notebook_utils.py
+++ scripts/notebook_utils.py
@@ -1,10 +1,21 @@
 import numpy as np
 from .train import *
+from .architecture import MLP, MLPManual
+import torchvision
+from torchvision.transforms import ToTensor, Normalize, Compose
 
-def tuneLearningRate_Torch(lr_array : np.array, optim: str, k:int, loss_type):
+transforms = Compose([
+    ToTensor(),
+    Normalize((0.1307,), (0.3081,))
+])
+
+trainset = torchvision.datasets.MNIST(root='data', train=True, download=True, transform=transforms)
+testset = torchvision.datasets.MNIST(root='data', train=False, download=True, transform=transforms)
+
+def tuneLearningRate_Torch(lr_array : np.array, optim: str, k:int, loss_type, loss_fn, device, batch_size, num_epochs, weight_decay):
     listofValAcc = []
     for learning_rate in lr_array:
-        print(f"Learning rate: {learn_rate}")
+        print(f"Learning rate: {learning_rate}")
         model = MLP(k, "BP", loss_type)
         if optim == "SGD":
             optimizer = torch.optim.SGD(model.parameters(), lr = learning_rate, weight_decay=weight_decay)
@@ -25,13 +36,13 @@ def tuneLearningRate_Torch(lr_array : np.array, optim: str, k:int, loss_type):
     print("Best learning rate is: ", best_lr)
     return best_lr
 
-def tuneLearningRate_Manual(lr_array : np.array, training_method: str, init_B: str, optim: str, k:int, loss_type):
+def tuneLearningRate_Manual(lr_array : np.array, training_method: str, init_B: str, optim: str, k:int, loss_type, loss_fn, device, num_epoch, weight_decay):
     listofValAcc = []
     for learning_rate in lr_array:
         print(f"Learning rate: {learning_rate}")
-        model = MLPManual(k, learning_rate, loss_type, training_method, init_B, optim, device, measure_alignment, False)
-        trainLostListLoc, trainAccListLoc, valLossListLoc, valAccListLoc ,_,_,_ = train_model_manually(model, k, trainset, testset, loss_type, loss_fn, 50,
-                                                                                                batch_size, momentum, nesterov_momentum, weight_decay, False, 
+        model = MLPManual(k * 784, learning_rate, loss_type, training_method, init_B, optim, device, False, True, False)
+        trainLostListLoc, trainAccListLoc, valLossListLoc, valAccListLoc ,_,_ = train_model_manually(model, k, trainset, testset, loss_type, loss_fn, num_epoch,
+                                                                                                128, False, False, weight_decay, False, 
                                                                                                 validate_model = True, device=device)
         last5 = valAccListLoc[-10:]
         meanOfLast5 = sum(last5) / len(last5)
@@ -64,7 +75,7 @@ def getMeanStd(model, k, trainset, testset, loss_type, loss_fn, num_epochs, batc
                 momentum, nesterov_momentum, weight_decay, measure_alignment, validate_model, device):
     results = {}
     for i in range(1,4):
-        trainLostListLoc, trainAccListLoc, valLossListLoc, valAccListLoc, _, _, _ = train_model_manually(model, k, trainset, testset, loss_type, loss_fn, num_epochs,
+        trainLostListLoc, trainAccListLoc, valLossListLoc, valAccListLoc, _, _, = train_model_manually(model, k, trainset, testset, loss_type, loss_fn, num_epochs,
                                                                                                 batch_size, momentum, nesterov_momentum, weight_decay, measure_alignment,
                                                                                                 validate_model = True, device=device)
         results[i] = valAccListLoc
